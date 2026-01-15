40e24be5f86bff7d0788f75e26a6de6a6490ca26
Nathan <nathanahoward1705@gmail.com>
2026-01-15 17:43:10 -0500

    Create Parameters header file

---
+++ include/Parameters.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct Parameters {
+    double preyBirthRate = 0.3;
+    double predatorDeathRate = 0.2;
+    double predationRate = 0.5;
+    double predatorBirthRate = 0.4;
+    double preyMovementRate = 0.5;
+    double predatorMovementRate = 0.3;
+};
