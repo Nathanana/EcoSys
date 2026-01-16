e332287a00a9cbb27eb14626e2700ae884236b01
Nathan <nathanahoward1705@gmail.com>
2026-01-16 08:25:08 -0500

    Setup makefile and build

---
+++ build/CMakeFiles/ecosys.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nathanana/Coding/EcoSys/src/DataLogger.cpp" "CMakeFiles/ecosys.dir/src/DataLogger.cpp.o" "gcc" "CMakeFiles/ecosys.dir/src/DataLogger.cpp.o.d"
+  "/Users/nathanana/Coding/EcoSys/src/Grid.cpp" "CMakeFiles/ecosys.dir/src/Grid.cpp.o" "gcc" "CMakeFiles/ecosys.dir/src/Grid.cpp.o.d"
+  "/Users/nathanana/Coding/EcoSys/src/Simulation.cpp" "CMakeFiles/ecosys.dir/src/Simulation.cpp.o" "gcc" "CMakeFiles/ecosys.dir/src/Simulation.cpp.o.d"
+  "/Users/nathanana/Coding/EcoSys/src/Visualizer.cpp" "CMakeFiles/ecosys.dir/src/Visualizer.cpp.o" "gcc" "CMakeFiles/ecosys.dir/src/Visualizer.cpp.o.d"
+  "/Users/nathanana/Coding/EcoSys/src/main.cpp" "CMakeFiles/ecosys.dir/src/main.cpp.o" "gcc" "CMakeFiles/ecosys.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ecosys.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecosys.dir/src/DataLogger.cpp.o"
+  "CMakeFiles/ecosys.dir/src/DataLogger.cpp.o.d"
+  "CMakeFiles/ecosys.dir/src/Grid.cpp.o"
+  "CMakeFiles/ecosys.dir/src/Grid.cpp.o.d"
+  "CMakeFiles/ecosys.dir/src/Simulation.cpp.o"
+  "CMakeFiles/ecosys.dir/src/Simulation.cpp.o.d"
+  "CMakeFiles/ecosys.dir/src/Visualizer.cpp.o"
+  "CMakeFiles/ecosys.dir/src/Visualizer.cpp.o.d"
+  "CMakeFiles/ecosys.dir/src/main.cpp.o"
+  "CMakeFiles/ecosys.dir/src/main.cpp.o.d"
+  "ecosys"
+  "ecosys.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ecosys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
